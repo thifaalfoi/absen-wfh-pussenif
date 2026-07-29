826198a3a347c75c34ff3316aa6da29b5f4d3e43
Thifaal <family42h@gmail.com>
2026-07-29 08:30:23 +0700

    feat: perbarui struktur kode dan file project

--- backend/aplikasi/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getVersion: () => ipcRenderer.invoke('app:version'),
-  reload: () => ipcRenderer.invoke('app:reload'),
-  minimize: () => ipcRenderer.invoke('app:minimize'),
-  maximize: () => ipcRenderer.invoke('app:maximize'),
-  close: () => ipcRenderer.invoke('app:close'),
-});
